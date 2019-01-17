b5bae96c8a131187d07855eea79e96b3c8dafb15
jeffvh <jh0791@gmail.com>
2019-01-17 22:06:02 +0000

    add basic layout

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Routes from './Routes';
 import NavBarContainer from './components/navbar/NavBarContainer';
+import { Layout } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <NavBarContainer />
-        <Routes />
-      </div>
+      <Layout className="layout">
+        <Header>
+          <NavBarContainer />
+        </Header>
+        <Content>
+          <Routes />
+        </Content>
+        <Footer>
+          Game On ©2019 Created by Jeffrey Huang
+        </Footer>
+      </Layout>
     );
   }
 }
