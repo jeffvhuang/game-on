500647e680b2bae2ec965c3a99be15efc6cfdd25
jeffvh <jh0791@gmail.com>
2019-04-07 21:56:07 +0100

    use mock data for epl fixtures

--- src/shared/redux/actions/epl-actions.js
+++ src/shared/redux/actions/epl-actions.js
@@ -5,7 +5,7 @@ import { youtubeAPI, eplAPI } from '../../../helpers/constants';
 import { sleep, sortEplSchedule } from '../../../helpers/utils';
 
 // Mock data
-import { TEAMS } from '../../../mockApiData/rapidEpl';
+import { TEAMS, SCHEDULE } from '../../../mockApiData/rapidEpl';
 // import { PLAYLIST } from '../../../mockApiData/EplYoutube';
 
 
@@ -47,41 +47,47 @@ export const getEplScheduleRequest = () => ({ type: A.GET_EPL_SCHEDULE_REQUEST }
 export const getEplScheduleSuccess = (payload, schedule) => ({ type: A.GET_EPL_SCHEDULE_SUCCESS, payload, schedule });
 export const getEplScheduleFailure = (err) => ({ type: A.GET_EPL_SCHEDULE_FAILURE, err });
 
-export const getEplSchedule = () => {
-  return (dispatch) => {
-    dispatch(getEplScheduleRequest());
-    return axios({
-      method: 'get',
-      url: eplAPI.HOST + eplAPI.SCHEDULE,
-      headers: {
-        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
-        'X-RapidAPI-Key': '9a04c3ec1dmshe9bb5802ba2545dp16f979jsndbae1452a5b5'
-      }
-    }).then(response => {
-      const fixturesObj = response.data.api.fixtures;
-      const eplSchedule = [];
-
-      Object.keys(fixturesObj).forEach(t => eplSchedule.push(fixturesObj[t]));
-
-      const sortedSchedule = sortEplSchedule(eplSchedule);
-
-      dispatch(getEplScheduleSuccess(sortedSchedule, eplSchedule));
-    }).catch(err => {
-      dispatch(getEplScheduleFailure(err));
-      throw(err);
-    });
-  };
-};
-
-// return mock data
 // export const getEplSchedule = () => {
-//   return async (dispatch) => {
+//   return (dispatch) => {
 //     dispatch(getEplScheduleRequest());
-//     await sleep(2000);
-//     const sortedSchedule = sortEplSchedule(SCHEDULE);
-//     return dispatch(getEplScheduleSuccess(sortedSchedule, SCHEDULE));
+//     return axios({
+//       method: 'get',
+//       url: eplAPI.HOST + eplAPI.SCHEDULE,
+//       headers: {
+//         'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
+//         'X-RapidAPI-Key': '9a04c3ec1dmshe9bb5802ba2545dp16f979jsndbae1452a5b5'
+//       }
+//     }).then(response => {
+//       const fixturesObj = response.data.api.fixtures;
+//       const eplSchedule = [];
+
+//       Object.keys(fixturesObj).forEach(t => eplSchedule.push(fixturesObj[t]));
+
+//       const sortedSchedule = sortEplSchedule(eplSchedule);
+
+//       dispatch(getEplScheduleSuccess(sortedSchedule, eplSchedule));
+//     }).catch(err => {
+//       dispatch(getEplScheduleFailure(err));
+//       throw(err);
+//     });
 //   };
 // };
+
+// return mock data
+export const getEplSchedule = () => {
+  return async (dispatch) => {
+    dispatch(getEplScheduleRequest());
+    await sleep(2000);
+    const fixturesObj = SCHEDULE;
+    const eplSchedule = [];
+
+    Object.keys(fixturesObj).forEach(t => eplSchedule.push(fixturesObj[t]));
+
+    const sortedSchedule = sortEplSchedule(eplSchedule);
+
+    return dispatch(getEplScheduleSuccess(sortedSchedule, eplSchedule));
+  };
+};
 
 // Get Teams
 export const getEplTeamsRequest = () => ({ type: A.GET_EPL_TEAMS_REQUEST });
