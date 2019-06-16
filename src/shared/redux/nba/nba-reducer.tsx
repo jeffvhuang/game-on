95e7bef4ac8d2674802c831c8a0623837e560515
Jeffrey Huang <jeffvh@outlook.com>
2019-06-16 12:11:43 +0100

    convert all nba redux actions, types and reducers to typescript and gather into it's own directory.

--- src/shared/redux/nba/nba-reducer.tsx
+++ src/shared/redux/nba/nba-reducer.tsx
@@ -1,4 +1,4 @@
-import { nbaActions as A } from '../actions/action-types';
+import * as C from './nba-constants';
 import { createYoutubeThumnailObjects } from '../../../helpers/utils';
 
 const initialState = {
@@ -9,40 +9,41 @@ const initialState = {
   upcoming: [],
   completed: [],
   videos: [],
-  thumbnails: []
+  thumbnails: [],
+  error: {}
 };
 
 function nbaReducer(state = initialState, action) {
   switch(action.type) {
-    case A.GET_NBA_SCHEDULE_REQUEST:
+    case C.GET_NBA_SCHEDULE_REQUEST:
       return Object.assign({}, state, { isFetching: true });
-    case A.GET_NBA_SCHEDULE_SUCCESS:
+    case C.GET_NBA_SCHEDULE_SUCCESS:
       return Object.assign({}, state,
         { 
           isFetching: false,
-          schedule: action.schedule,
-          gamesToday: action.payload.gamesToday,
-          upcoming: action.payload.upcoming,
-          completed: action.payload.beforeToday
+          schedule: action.payload,
+          gamesToday: action.sortedSchedule.gamesToday,
+          upcoming: action.sortedSchedule.upcoming,
+          completed: action.sortedSchedule.beforeToday
         });
-    case A.GET_NBA_SCHEDULE_FAILURE:
+    case C.GET_NBA_SCHEDULE_FAILURE:
       return Object.assign({}, state, { isFetching: false, error: action.err });
     
-    case A.GET_NBA_TEAMS_REQUEST:
+    case C.GET_NBA_TEAMS_REQUEST:
       return Object.assign({}, state, { isFetching: true });
-    case A.GET_NBA_TEAMS_SUCCESS:
+    case C.GET_NBA_TEAMS_SUCCESS:
       return Object.assign({}, state, { isFetching: false, teams: action.payload });
-    case A.GET_NBA_TEAMS_FAILURE:
+    case C.GET_NBA_TEAMS_FAILURE:
       return Object.assign({}, state, { isFetching: false, error: action.err });
     
-    case A.GET_NBA_VIDEOS_REQUEST:
+    case C.GET_NBA_VIDEOS_REQUEST:
       return Object.assign({}, state, { isFetching: true });
-    case A.GET_NBA_VIDEOS_SUCCESS:
+    case C.GET_NBA_VIDEOS_SUCCESS:
       return Object.assign({}, state, { 
         isFetching: false,
         videos: action.payload,
         thumbnails: createYoutubeThumnailObjects(action.payload) });
-    case A.GET_NBA_VIDEOS_FAILURE:
+    case C.GET_NBA_VIDEOS_FAILURE:
       return Object.assign({}, state, { isFetching: false, error: action.err });
 
     default:
