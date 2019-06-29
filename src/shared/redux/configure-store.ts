e2ced810972b4179cf03cc791412631f14585048
Jeffrey Huang <jeffvh@outlook.com>
2019-06-29 14:25:32 +0100

    add redux dev tools extension from npm

--- src/shared/redux/configure-store.ts
+++ src/shared/redux/configure-store.ts
@@ -1,29 +1,20 @@
-import { createStore, compose, applyMiddleware, Store } from 'redux';
+import { createStore, applyMiddleware, Store } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import loggerMiddleware from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './root-reducer';
 import { ReduxState } from './redux-state';
 
-// const store = createStore<ReduxState, any, any, any>(
-//   rootReducer,
-//   undefined,
-//   composeEnhancers(
-//     applyMiddleware(thunk, logger)
-//   )
-// );
+// Create a configure store function of type `ReduxState`
+export default function configureStore(preloadedState): Store<ReduxState> {
+  const middlewares = [loggerMiddleware, thunkMiddleware];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+  
+  const enhancers = [middlewareEnhancer];
+  const composeEnhancers = composeWithDevTools(...enhancers);
 
-// export default store;
-
-// Create a configure store function of type `IAppState`
-export default function configureStore(): Store<ReduxState> {
-  const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunkMiddleware);
-  // const composeEnhancers = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose || compose;
-  const composeEnhancers = compose(middlewareEnhancer);
-  // const initialState = {};
-  const initialState = undefined;
-
-  const store = createStore(rootReducer, initialState, composeEnhancers);
+  const store = createStore(rootReducer, preloadedState, composeEnhancers);
 
   return store;
 }
