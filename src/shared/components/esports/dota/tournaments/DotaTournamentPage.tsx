ce01e8806e108e2d7177ea9d239c3f8b4b80e184
Jeffrey Huang <jeffvh@outlook.com>
2020-01-24 11:07:10 +1100

    separate dota tournament page into separate react components

--- src/shared/components/esports/dota/tournaments/DotaTournamentPage.tsx
+++ src/shared/components/esports/dota/tournaments/DotaTournamentPage.tsx
@@ -6,13 +6,8 @@ import { getDotaSeriesTournaments } from "../../../../redux/dota/dota-actions";
 import { DotaState } from "../../../../redux/dota/dota-types";
 import { ReduxState } from "../../../../redux/redux-state";
 import { ESportsTournament } from "../../../../../types/esports-api/esports-tournament.model";
-import {
-  capitalise,
-  getDateWithOrdinal,
-  getFormattedTime
-} from "../../../../../helpers/utils";
-import { ESportsMatchBase } from "../../../../../types/esports-api/esports-match-base.model";
-import { ESportsTeamBase } from "../../../../../types/esports-api/esports-team-base.model";
+import DotaTournamentPageTeams from "./DotaTournamentPageTeams";
+import DotaTournamentPageMatches from "./DotaTournamentPageMatches";
 
 interface MatchParams {
   id: string;
@@ -51,83 +46,20 @@ class DotaTournamentPage extends React.Component<Props, State> {
     return tournaments.length ? tournaments[0].league.name : "";
   };
 
-  capitaliseParts = (bestOf: string): string => {
-    const parts = bestOf.split("_");
-    const capitalisedParts = parts.map(p => capitalise(p));
-    return capitalisedParts.join(" ");
-  };
-
-  getStatus = (match: ESportsMatchBase, teams: ESportsTeamBase[]): string => {
-    if (match.status === "not_started") {
-      console.log(match.beginAt);
-      return (
-        this.parseDate(match.beginAt) +
-        " - " +
-        getFormattedTime(new Date(match.beginAt))
-      );
-    } else if (match.status === "finished") {
-      if (match.draw) return "Draw";
-      const winner = teams.find(t => t.id == match.winnerId);
-      return winner ? winner.acronym + " Wins" : "Match Finished";
-    } else {
-      return this.capitaliseParts(match.status);
-    }
-  };
-
-  parseDate = (date: string): string => {
-    const d = new Date(date);
-    return getDateWithOrdinal(d);
-  };
-
   render() {
-    const { dota } = this.props;
     return (
       <div className="section content">
         <div className="page-header">
-          <h1>{this.getTournamentName(dota.selectedSeriesTournaments)}</h1>
+          <h1>
+            {this.getTournamentName(this.props.dota.selectedSeriesTournaments)}
+          </h1>
         </div>
         <div>
-          {dota.selectedSeriesTournaments.map(tournament => (
+          {this.props.dota.selectedSeriesTournaments.map(tournament => (
             <div key={tournament.id} className="tournament-section">
               <h2>{tournament.name}</h2>
-              <div className="team-section">
-                <h3>Teams</h3>
-                <div className="teams-list">
-                  {tournament.teams.map(team => (
-                    <div key={team.id} className="team-name">
-                      <h4>{team.name}</h4>
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <div>
-                <h3>Matches</h3>
-                <div className="match-list">
-                  <div className="match match-header-row">
-                    <div>
-                      <h4>Matchup</h4>
-                    </div>
-                    <div className="match-type">Game Type</div>
-                    <div>Status</div>
-                  </div>
-                </div>
-                <div className="match-list">
-                  {tournament.matches.map(match => (
-                    <div key={match.id} className="match">
-                      <div>
-                        <h4>{match.name}</h4>
-                      </div>
-                      <div className="match-type">
-                        {this.capitaliseParts(match.matchType)}{" "}
-                        {match.numberOfGames.toString()}
-                      </div>
-                      <div className="match-status">
-                        {this.getStatus(match, tournament.teams)}
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
+              <DotaTournamentPageTeams tournament={tournament} />
+              <DotaTournamentPageMatches tournament={tournament} />
             </div>
           ))}
         </div>
---
+++ src/shared/components/esports/dota/tournaments/DotaTournamentPageTeams.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import { ESportsTournament } from "../../../../../types/esports-api/esports-tournament.model";
+
+interface Props {
+  tournament: ESportsTournament;
+}
+
+function DotaTournamentPageTeams({ tournament }: Props) {
+  return (
+    <div className="team-section">
+      <h3>Teams</h3>
+      <div className="teams-list">
+        {tournament.teams.map(team => (
+          <div key={team.id} className="team-name">
+            <h4>{team.name}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default DotaTournamentPageTeams;
